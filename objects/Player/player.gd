5406ae77a7b95a28e3f0e2e3ff27264661566b0e
TdubMorris <95392519+TdubMorris@users.noreply.github.com>
2025-10-02 00:11:25 -0500

    added double jumping

--- objects/Player/player.gd
+++ objects/Player/player.gd
@@ -4,7 +4,9 @@ extends CharacterBody2D
 @export var jump_height : float = 20
 @export var jump_time : float = 0.5
 @export var accel : float = 30
-@export var max_speed : float = 120
+@export var max_speed : float = 100
+@export var max_fall_speed : float = 240
+@export var extra_jumps : int = 1
 
 @onready var sprite = %PlayerSprite
 @onready var buffer = %BufferTimer
@@ -18,7 +20,7 @@ var was_on_ground : bool = false
 @onready var jump_vel : float = -1.0 * ((4.0 * jump_height)/jump_time)
 
 func _physics_process(delta: float) -> void:
-	velocity.y += delta * gravity
+	velocity.y = min(max_fall_speed, velocity.y + delta * gravity)
 	velocity.x = move_toward(velocity.x, max_speed * Input.get_axis("left", "right"), accel)
 	
 	if Input.is_action_just_pressed("jump"):
@@ -26,9 +28,17 @@ func _physics_process(delta: float) -> void:
 		if !coyote.is_stopped():
 			velocity.y = jump_vel
 			coyote.stop()
-	if is_on_floor() and !buffer.is_stopped():
-		velocity.y = jump_vel
-		buffer.stop()
+			sprite.play("jump")
+		elif !is_on_floor() and jumps_remaining > 0:
+			velocity.y = jump_vel
+			sprite.play("doublejump")
+			jumps_remaining -= 1
+	if is_on_floor():
+		jumps_remaining = extra_jumps
+		if !buffer.is_stopped():
+			velocity.y = jump_vel
+			sprite.play("jump")
+			buffer.stop()
 	
 	was_on_ground = is_on_floor()
 	move_and_slide()
@@ -46,8 +56,6 @@ func _physics_process(delta: float) -> void:
 			sprite.play("walk")
 	if Input.get_axis("left", "right") == 0 and is_on_floor():
 		sprite.play("idle")
-	if Input.is_action_just_pressed("jump"):
-		sprite.play("jump")
 	if velocity.y > 0:
 		sprite.play("fall")
 	
